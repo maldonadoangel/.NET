e9cc2865060019ad6bb50714869b2dc5c9ce6cd7
Angel Morales <maldonadoangel7069@gmail.com>
2022-08-18 15:13:50 -0600

    Sintaxis

---
+++ MyWinFormsAppSol/MyWinFormsApp1/Form1.cs
@@ -0,0 +1,10 @@
+namespace MyWinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TiposDeDatos/TiposDeDatos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDeDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = String.Empty;
+            Console.WriteLine("Proporciona un mensaje por favor: ");
+            message = Console.ReadLine();
+            Console.WriteLine("Tu cadena es : " + message);
+            Console.ReadKey();
+        }
+    }
+}
