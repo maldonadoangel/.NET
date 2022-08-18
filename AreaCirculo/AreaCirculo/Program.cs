d16bb76023b15fce5c9ffb660df9a5b25ae9fb51
Angel Morales <maldonadoangel7069@gmail.com>
2022-08-18 16:29:55 -0600

    variables, constantes y una forma de convertir texto en decimales

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cuando usamos constantes se recomienda escribir el nombre
+            //en mayusculas
+            const double PI = 3.1416;
+            double radio;
+            string userData;
+            double areaCirculo;
+
+            Console.Write("Cual es el radio del circulo: ");
+            userData = Console.ReadLine();
+
+            if(double.TryParse(userData, out radio))
+            {
+                areaCirculo = PI * Math.Pow(radio, 2);
+                //Forma de concatenar variables con valores numericos
+                Console.WriteLine($"El area del circulo es: {areaCirculo}");
+            }
+            else
+            {
+                Console.WriteLine("El valor introducido no es numerico.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
