403c6fc1494b8e7a9e7dd8e16bc564780f15bed1
Angel Morales <maldonadoangel7069@gmail.com>
2022-09-24 22:54:43 -0600

    Ciclo While y uso de Listas

---
+++ CicloWhile/CicloWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string? nombre;
+int espacio = 0;
+List<string> mochila = new List<string>();
+Console.Write("Cuantos espacios desea en su mochila?  " );
+espacio = int.Parse(Console.ReadLine());
+
+
+//Utilizamos este ciclo while, para guardar los objetos en la mochila
+while(mochila.Count < espacio)
+{
+    Console.Write("Ingrese el nombre del objeto a guardar " );
+    nombre = Console.ReadLine();
+    mochila.Add(nombre);
+}
+
+//Bucle Determinado, utilizamos este for para mostrar los objetos guardados
+for(int i = 0; i < mochila.Count; i++)
+{
+    Console.WriteLine("--------");
+    Console.WriteLine(mochila[i]);
+}
