4a214438c9d66efd81f17df74e37401bceaceed3
Angel Morales <maldonadoangel7069@gmail.com>
2022-09-25 08:58:53 -0600

    Manejo de Excepciones 1

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero = 0, contador = 0, numeroRandom;
+//Generamos un numero random desde 0 a 10
+Random random = new Random();
+//Asignamos el valor Random a la variable numero random, convirtiendolo en entero al asignar el parametros de 0 a 10
+numeroRandom = random.Next(0, 10);
+Console.WriteLine("Bienvenido al programa donde adivinaras el numero de la PC");
+
+
+
+do
+{
+    contador++;
+    Console.WriteLine("Ingrese un numero para adivinar: ");
+    try
+    {
+        numero = Convert.ToInt32(Console.ReadLine());
+    }catch(FormatException ex)
+    {
+        Console.WriteLine("No haz introducido un valor numerico valido, se tomara como valor por defecto el 0");
+    }
+    
+    if(numero > numeroRandom)
+    {
+        Console.WriteLine($"El numero ingresado es mayor al de la maquina, intenta de nuevo");
+    }
+    if(numero < numeroRandom){
+        Console.WriteLine("El numero ingresado es menor al de la maquina, intenta de nuevo");
+    }
+    
+} while (numero != numeroRandom);
+
+Console.WriteLine($"Correcto!!, el numero es: {numeroRandom} y el numero de intentos es de {contador}");
