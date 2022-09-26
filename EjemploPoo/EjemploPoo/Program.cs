4126077c6fe4a44515bc338647ef3e791814b363
Angel Morales <maldonadoangel7069@gmail.com>
2022-09-26 12:36:44 -0600

    Programacion Orientada a Objetos, clases y atributos

---
+++ EjemploPoo/EjemploPoo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Circulo circulo = new Circulo();
+
+Console.WriteLine($"El calculo del area es: {circulo.calculoArea(5)}"); 
+
+Circulo circulo2 = new Circulo();
+
+Console.WriteLine($"{circulo2.calculoArea(9)}");
+
+
+class Circulo
+{
+    const double pi = 3.1416; // propiedad de la clase circulo
+
+
+   public double calculoArea(int radio)
+    {
+        return pi * radio * radio;
+    }
+
+}
