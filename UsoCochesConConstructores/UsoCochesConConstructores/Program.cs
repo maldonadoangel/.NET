1740dd8a72219b1738ef3f3ac6cedf22c3afde6f
Angel Morales <maldonadoangel7069@gmail.com>
2022-09-27 10:25:04 -0600

    Getters y Setters

--- UsoCochesConConstructores/UsoCochesConConstructores/Program.cs
+++ UsoCochesConConstructores/UsoCochesConConstructores/Program.cs
@@ -9,7 +9,10 @@ Coche carro2 = new Coche();
 Console.WriteLine($"{carro.getInformacionCoche()}");
 Console.WriteLine($"{carro2.getInformacionCoche()}");
 Coche carro3 = new Coche(4500.54, 47.14);
-Console.WriteLine(carro3.getInformacionCoche());
+Console.WriteLine($"El carro 3 tiene: {carro3.getInformacionCoche()}");
+
+carro3.setExtras(true, "Cuero");
+Console.WriteLine(carro3.getExtras());
 
 class Coche
 {
@@ -32,15 +35,24 @@ class Coche
 
     public Coche(double largoCoche, double anchoCoche)
     {
-        ruedas = 4;
-        longitud = largoCoche;
-        ancho = anchoCoche;
+        this.ruedas = 4;
+        this.longitud = largoCoche;
+        this.ancho = anchoCoche;
     }
 
-    //metodos que nos dan informacion de propiedades, el metodo get se encarga de devolvernos o darnos la informacion que queremos, se recomienda
-    //Inicializar los nombres de estos metodos que devuelven informacion con getNombreDelMetodo
     public string getInformacionCoche()
     {
-        return "El carro tiene: " + this.ruedas + " Con una Longitud de: " + this.longitud + " con un ancho de: " + this.ancho;
+        return "" + this.ruedas + " Con una Longitud de: " + this.longitud + " con un ancho de: " + this.ancho;
+    }
+    //metodo setter, no devuelve nada, solo modifica los valores o da valores, pero no los devuelve, por lo tanto se utiliza void
+    public void setExtras(bool climatizador, string tapiceria)
+    {
+        this.climatizador = climatizador;
+        this.tapiceria = tapiceria;
+    }
+
+    public string getExtras()
+    {
+        return "Extras del coche:" + " tapiceria: " + this.tapiceria + "climatizador: " + this.climatizador; 
     }
 }
