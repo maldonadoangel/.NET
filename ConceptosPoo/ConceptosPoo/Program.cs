ebaded7e03373ac45812ed40b179fdfad5d05452
Angel Morales <maldonadoangel7069@gmail.com>
2022-09-27 17:27:28 -0600

    llamadas y clase math

---
+++ ConceptosPoo/ConceptosPoo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptosPoo;
+
+Console.WriteLine("Hello, World!");
+
+
+realizarTarea();
+//Espera a que le teclees algo
+Console.ReadKey();
+
+static void realizarTarea()
+{
+    Punto punto2 = new Punto(150, 90);
+    Punto punto = new Punto();
+
+    double distancia = punto.DistanciaHasta(punto2);
+    Console.WriteLine($"La distancia entre los dos puntos es: {distancia}");
+}
---
+++ ConceptosPoo/ConceptosPoo/Punto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPoo
+{
+    internal class Punto
+    {
+        //atributos
+        private int x, y;
+
+        //Constructores
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Punto()
+        {
+            this.x=0;
+            this.y=0;
+        }
+
+        public double DistanciaHasta(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+            int yDif = this.y - otroPunto.y;   
+
+            //Teorema de pitagoras
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
+            return distanciaPuntos;
+        }
+    }
+
+    
+}
