06eab3efbd01b8fcc2a875f4f415e923b384eb00
Angel Morales <maldonadoangel7069@gmail.com>
2022-09-28 05:48:12 -0600

    Metodo Static

--- ConceptosPoo/ConceptosPoo/Program.cs
+++ ConceptosPoo/ConceptosPoo/Program.cs
@@ -12,7 +12,11 @@ static void realizarTarea()
 {
     Punto punto2 = new Punto(150, 90);
     Punto punto = new Punto();
+    Punto punto3 = new Punto();
 
     double distancia = punto.DistanciaHasta(punto2);
     Console.WriteLine($"La distancia entre los dos puntos es: {distancia}");
+
+    //Nos mostrara 3 objetos debido a que creamos 3 objetos de tipo Punto en esta clase, por lo tanto nuestro atributo static toma los valores dentro de su clase padre.
+    Console.WriteLine($"Numero de objetos creados: {Punto.ContadorDeObjetos()}");
 }
--- ConceptosPoo/ConceptosPoo/Punto.cs
+++ ConceptosPoo/ConceptosPoo/Punto.cs
@@ -10,18 +10,21 @@ namespace ConceptosPoo
     {
         //atributos
         private int x, y;
+        private static int contadorDeObjetos = 0;
 
         //Constructores
         public Punto(int x, int y)
         {
             this.x = x;
             this.y = y;
+            contadorDeObjetos++;
         }
 
         public Punto()
         {
             this.x=0;
             this.y=0;
+            contadorDeObjetos++;
         }
 
         public double DistanciaHasta(Punto otroPunto)
@@ -33,6 +36,15 @@ namespace ConceptosPoo
             double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
             return distanciaPuntos;
         }
+
+     /*   public static int ContadorDeObjetos()
+        {
+            return contadorDeObjetos;
+        }
+     */
+
+        // al ser static pertenece a la clase Punto y no es un metodo como tal
+        public static int ContadorDeObjetos()=> contadorDeObjetos;
     }
 
     
