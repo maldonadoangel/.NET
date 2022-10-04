9498276f33c2b6f60052df08656844ccffbef873
Angel Morales <maldonadoangel7069@gmail.com>
2022-10-04 17:28:06 -0600

    Comenzando con los arreglos

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int[] numeros = new int[5];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
--- ConceptosPoo/ConceptosPoo/Program.cs
+++ ConceptosPoo/ConceptosPoo/Program.cs
@@ -1,13 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConceptosPoo;
+using static System.Math;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 
-realizarTarea();
+//realizarTarea();
 //Espera a que le teclees algo
+//como importamos system.Math, como es statico porque ponemos primero el Math que es el nombre de la clase, al importarlo ya no es necesario colocar
+//Math.sqrt(9), sino simplemente Sqrt(9)
+double raiz = Sqrt(9);
+double potencia = Pow(3, 4);
 Console.ReadKey();
+Console.WriteLine(raiz);
+Console.Write(potencia);
 
+//--------------------------------------------------------------------------------------------------
+//Clases anonimas
+var nombreVariable = new {Nombre = "Angel", Edad = 24 };
+Console.WriteLine($"Tu nombre es: {nombreVariable.Nombre} y tu edad es de: {nombreVariable.Edad}");
+//--------------------------------------------------------------------------------------------------
 static void realizarTarea()
 {
     Punto punto2 = new Punto(150, 90);
@@ -18,5 +30,6 @@ static void realizarTarea()
     Console.WriteLine($"La distancia entre los dos puntos es: {distancia}");
 
     //Nos mostrara 3 objetos debido a que creamos 3 objetos de tipo Punto en esta clase, por lo tanto nuestro atributo static toma los valores dentro de su clase padre.
+    //Como observamos al ser un metodo estatico, debemos llamar la clase primero seguido del punto y el metodo static
     Console.WriteLine($"Numero de objetos creados: {Punto.ContadorDeObjetos()}");
 }
