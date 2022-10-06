0ffdf1617a657241712eb7b2b0cbbdde8bc449a4
Angel Morales <maldonadoangel7069@gmail.com>
2022-10-05 20:19:20 -0600

    Arreglos con funciones

---
+++ ArreglosConFunciones/ArreglosConFunciones/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] nombresAlumnos = { "Angel", "Pepito", "Mario", "Juan", "Carlos" };
+Console.ForegroundColor = ConsoleColor.Green;
+mostrarNombreAlumno(nombresAlumnos);
+
+
+   static void mostrarNombreAlumno(string[] nombreAlumno)
+{
+    foreach(string elemento in nombreAlumno)
+    {
+        Console.WriteLine(elemento);    
+    }
+}
