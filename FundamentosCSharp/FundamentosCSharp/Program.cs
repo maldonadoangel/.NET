7104d81592c8b7fa3ff7af8930f93308211c1896
Angel Morales <maldonadoangel7069@gmail.com>
2022-10-11 17:46:07 -0600

    Algunos fundamentos de c#

---
+++ FundamentosCSharp/FundamentosCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+sbyte numero = -128;
+ulong numeroLargo = 12120021545154454449;
+float numeroDecimal = 14.6f;
+double numeroDec = 54.5;
+object persona = new { nombre = "Angel", apellido = "Morales", edad = 24 };
+Console.WriteLine(persona);
+
+
+DateTime fecha = DateTime.Now;
+
+Console.ReadKey();
+
+
+Console.WriteLine($"La fecha actual es: {fecha}");
