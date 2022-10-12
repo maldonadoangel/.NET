28727a9e588cc42d50cd9359f97d3679a1a97b5d
Angel Morales <maldonadoangel7069@gmail.com>
2022-10-11 19:16:08 -0600

    Constructor y clase bebida

---
+++ PooBebida/PooBebida/Models/Bebida.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooBebida.Models
+{
+    internal class Bebida
+    {
+        //Atributos
+        public string nombre { get; set; }
+        public string sabor { get; set; }
+
+        public int cantidad { get; set; }
+
+        //Constructores o inicializador de atributos
+    
+        public Bebida(string nombre, string sabor, int cantidad){
+
+                this.nombre = nombre;
+                this.sabor = sabor;
+                this.cantidad = cantidad;
+            }
+
+        public void beberse(int cuantoBebio)
+        {
+            this.cantidad = this.cantidad - cuantoBebio;
+        } 
+        
+    }
+}
---
+++ PooBebida/PooBebida/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PooBebida.Models;
+
+Console.WriteLine("Hello, World!");
+
+Bebida bebida2 = new Bebida("Grapete", "Uva", 1000);
+bebida2.beberse(500);
+Console.WriteLine($"La bebida tiene un peso de: {bebida2.cantidad} ml actualmente, debido a que bebiste 500 ml");
+
