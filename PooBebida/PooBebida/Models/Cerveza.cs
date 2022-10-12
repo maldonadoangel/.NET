a733f2140a59672e377668d4ac846025103f449b
Angel Morales <maldonadoangel7069@gmail.com>
2022-10-11 19:35:57 -0600

    Herencia

---
+++ PooBebida/PooBebida/Models/Cerveza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooBebida.Models
+{
+    //Herencia
+    internal class Cerveza: Bebida
+    {
+        public Cerveza(string nombre, string sabor = "Sabor no ingresado", int cantidad = 100) 
+            :base(nombre, sabor, cantidad)//Como podemos ver de esta forma aplicamos la herencia de la clase padre Bebida
+        {
+
+        }
+    }
+}
--- PooBebida/PooBebida/Program.cs
+++ PooBebida/PooBebida/Program.cs
@@ -7,3 +7,8 @@ Bebida bebida2 = new Bebida("Grapete", "Uva", 1000);
 bebida2.beberse(500);
 Console.WriteLine($"La bebida tiene un peso de: {bebida2.cantidad} ml actualmente, debido a que bebiste 500 ml");
 
+
+//Herencia 
+Cerveza cerveza = new Cerveza("Gallo", "Amargo", 200);
+cerveza.beberse(100);
+Console.WriteLine($"La cantidad que le queda a su cerveza es de: {cerveza.cantidad} ML");
